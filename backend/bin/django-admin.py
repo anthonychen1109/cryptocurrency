3e2820779f051a939e5c9e07864bb7cca1921fd1
ac065455 <chen.anthony.1109@gmail.com>
2018-04-14 21:30:56 -0400

    adding django backend

---
+++ backend/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anthonychen/Desktop/projects/Redux-Projects/cryptocurrency/cryptocurrency/backend/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
