3c2106ac774672925e127fdfa806a14d86b1258a
ac065455 <chen.anthony.1109@gmail.com>
2018-04-16 17:37:21 -0400

    added changes to fetch_coin reducer

--- src/reducers/reducer_fetch_coins.js
+++ src/reducers/reducer_fetch_coins.js
@@ -1,10 +1,17 @@
 import { FETCH_COINS } from '../actions';
 
-export default function(state = {}, action) {
+const initialState = {
+  coin_list: []
+}
+
+export default function(state = initialState, action) {
   switch(action.type) {
     case FETCH_COINS:
-      console.log(action.payload);
-      return action.payload.data
+      console.log('reducer', action.payload.data);
+      return {
+        ...state,
+        coin_list: [...state, action.payload.data]
+      }
     default:
       return state
   }
