93e4bdb20c190c9f11a18cb8b671c322f830a416
ac065455 <chen.anthony.1109@gmail.com>
2018-04-17 15:11:41 -0400

    added fetch all coins

--- src/reducers/reducer_fetch_coins.js
+++ src/reducers/reducer_fetch_coins.js
@@ -1,16 +1,23 @@
-import { FETCH_COINS } from '../actions';
+import { FETCH_COINS, FETCH_ALL_COINS } from '../actions';
 
 const initialState = {
-  coin_list: []
+  coin_list: [],
+  coin_list_all: []
 }
 
 export default function(state = initialState, action) {
   switch(action.type) {
     case FETCH_COINS:
-      console.log('reducer', action.payload.data);
+      // console.log('reducer', action.payload.data);
       return {
         ...state,
         coin_list: action.payload.data
+      }
+    case FETCH_ALL_COINS:
+      console.log('reducer', action.payload.data);
+      return {
+        ...state,
+        coin_list_all: action.payload.data
       }
     default:
       return state
