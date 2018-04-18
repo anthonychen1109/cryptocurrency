dddfee7ba943f5df156e9b0a50fa95869adb1761
ac065455 <chen.anthony.1109@gmail.com>
2018-04-18 17:27:19 -0400

    adding action to retrieve coin by symbol

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.coinmarketcap.com/v1/';
+const BACKEND_URL = 'http://127.0.0.1:8000/';
 
 export const FETCH_COINS = 'FETCH_COINS';
 export const FETCH_ALL_COINS = 'FETCH_ALL_COINS';
+export const FETCH_HISTORICAL_DATA = 'FETCH_HISTORICAL_DATA';
 
 export function fetchCoins() {
   const url = `${BASE_URL}ticker/?limit=10`;
@@ -28,4 +30,16 @@ export function fetchAllCoins() {
     payload: request
   }
 
+}
+
+export function fetchHistoricalData() {
+  const url =`${BACKEND_URL}coins/`;
+
+  const request = axios.get(url);
+  console.log('action', request);
+
+  return {
+    type: FETCH_HISTORICAL_DATA,
+    payload: request
+  }
 }
