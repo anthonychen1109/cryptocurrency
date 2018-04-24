459731d14d0be338ba243901278b28e822d79b86
ac065455 <chen.anthony.1109@gmail.com>
2018-04-23 21:16:07 -0400

    added single coin search

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ const BACKEND_URL = 'http://127.0.0.1:8000/';
 export const FETCH_COINS = 'FETCH_COINS';
 export const FETCH_ALL_COINS = 'FETCH_ALL_COINS';
 export const FETCH_HISTORICAL_DATA = 'FETCH_HISTORICAL_DATA';
+export const SEARCH_COIN = 'SEARCH_COIN';
 
 export function fetchCoins() {
   const url = `${BASE_URL}ticker/?limit=10`;
@@ -41,6 +42,17 @@ export function fetchHistoricalData(sym) {
 
   return {
     type: FETCH_HISTORICAL_DATA,
+    payload: request
+  }
+}
+
+export function searchCoin(name) {
+
+  const url = `${BASE_URL}ticker/${name}/`
+  const request = axios.get(url);
+
+  return {
+    type: SEARCH_COIN,
     payload: request
   }
 }
