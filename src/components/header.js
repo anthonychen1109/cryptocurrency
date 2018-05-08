b02bc5e3478a27684ea081259a1bac684f477113
ac065455 <chen.anthony.1109@gmail.com>
2018-05-08 14:47:43 -0400

    fixed name

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <div className="header">
       <ul>
-        <li><Link className='text' to='/'>WTFCOIN</Link></li>
+        <li><Link className='text' to='/'>Lambo to the Moon</Link></li>
         <li><Link className='text' to='/markets'>Markets</Link></li>
         <li><Link className='text' to='/search'>Search Coin</Link></li>
       </ul>
       <ul>
-        <li><a className='text' href='http://127.0.0.1:8000/login/'>Log In</a></li>
+        <li><Link className='text' to='/login'>Log In</Link></li>
         <li><Link className='text' to='/signup'>Sign Up</Link></li>
       </ul>
     </div>
--- src/components/routes.js
+++ src/components/routes.js
@@ -5,11 +5,13 @@ import App from './app';
 import Signup from '../containers/signup';
 import Markets from '../containers/markets';
 import SingleDisplayCoin from '../containers/single_display_coin';
+import Login from '../containers/login';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path='/login' component={Login} />
         <Route path='/signup' component={Signup} />
         <Route path='/markets' component={Markets} />
         <Route path='/search' component={SingleDisplayCoin} />
